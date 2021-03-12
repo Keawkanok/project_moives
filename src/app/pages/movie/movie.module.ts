fafe4d3e5cfe86f0bac36d8a6df8df2be9811e7c
keawkanok <thanyakon.ke.61@ubu.ac.th>
2021-03-12 20:02:07 +0700

    filenew

--- src/app/pages/movie/movie.module.ts
+++ src/app/pages/movie/movie.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { MoviePageRoutingModule } from './movie-routing.module';
 
 import { MoviePage } from './movie.page';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MoviePageRoutingModule
+    MoviePageRoutingModule,
+    HttpClient,
   ],
   declarations: [MoviePage]
 })
